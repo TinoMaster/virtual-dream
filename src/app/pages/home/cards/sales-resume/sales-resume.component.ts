40f93d065a6facc497e89827ee036c2083e6bee0
TinoMaster <ommallono@gmail.com>
2024-11-30 16:59:53 +0100

    created card sales resume

--- src/app/pages/home/cards/sales-resume/sales-resume.component.ts
+++ src/app/pages/home/cards/sales-resume/sales-resume.component.ts
@@ -1,9 +1,17 @@
-import { CurrencyPipe, TitleCasePipe } from '@angular/common';
+import { CurrencyPipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sales-resume',
-  imports: [CurrencyPipe, TitleCasePipe],
+  imports: [
+    CurrencyPipe,
+    TitleCasePipe,
+    UpperCasePipe,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './sales-resume.component.html',
   styleUrl: './sales-resume.component.css',
 })
