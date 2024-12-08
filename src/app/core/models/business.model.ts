83da8a1008d9e7310395f271f6cf462ccc49a245
TinoMaster <ommallono@gmail.com>
2024-12-08 20:18:08 +0100

    added some stuff to the BusinessModel

--- src/app/core/models/business.model.ts
+++ src/app/core/models/business.model.ts
@@ -2,4 +2,9 @@ export interface BusinessModel {
   id: number;
   name: string;
   description: string;
+  address: string;
+  phone?: string;
+  email: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
